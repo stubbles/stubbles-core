6f492373e57f01c2dad1bb21c9e1604eaae740d2
Frank Kleine <mikey@stubbles.net>
2012-01-15 22:44:59 +0100

    more infrastructure

--- src/test/bootstrap.php
+++ src/test/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
+if (file_exists(__DIR__ . '/../../vendor/.composer/autoload.php') === false) {
+    die('Please run "composer.phar install" first' . "\n");
+}
+
 require __DIR__ . '/../../vendor/.composer/autoload.php';
 ?>
