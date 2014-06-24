4bb1799a6f53648fa5a8e128191ae221da7455dd
mikey179 <mikey@stubbles.net>
2014-06-24 17:07:03 +0200

    possibility to add callables to composite stream filter

--- src/main/php/streams/filter/CompositeStreamFilter.php
+++ src/main/php/streams/filter/CompositeStreamFilter.php
@@ -8,6 +8,7 @@
  * @package  stubbles
  */
 namespace stubbles\streams\filter;
+use stubbles\lang\exception\IllegalArgumentException;
 /**
  * Composite to combine a list of stream filters.
  *
@@ -25,12 +26,20 @@ class CompositeStreamFilter implements StreamFilter
     /**
      * add a stream filter
      *
-     * @param   StreamFilter  $streamFilter
+     * @param   StreamFilter|callable  $streamFilter
      * @return  CompositeStreamFilter
+     * @throws  IllegalArgumentException
      */
-    public function addStreamFilter(StreamFilter $streamFilter)
+    public function addStreamFilter($streamFilter)
     {
-        $this->streamFilter[] = $streamFilter;
+        if ($streamFilter instanceof StreamFilter) {
+            $this->streamFilter[] = $streamFilter;
+        } elseif (is_callable($streamFilter)) {
+            $this->streamFilter[] = new CallableStreamFilter($streamFilter);
+        } else {
+            throw new IllegalArgumentException('Given stream filter is neither a callable nor an instance of stubbles\streams\filter\StreamFilter');
+        }
+
         return $this;
     }
 
